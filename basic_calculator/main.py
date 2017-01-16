d5e184eea16721e181a3fdd6bd88a32a0524a0b1
Pajan <hessaum.sadri@gmail.com>
2017-01-16 00:48:17 +0000

    Basic Calculator is working!

--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -2,4 +2,13 @@
 
 
 def basic_calculator(num1, num2, operation):
-    pass
+    if operation.lower() == 'add':
+        return (num1 + num2)
+    elif operation.lower() == 'subtract':
+        return (num1 - num2)
+    elif operation.lower() == 'multiply':
+        return (num1 * num2)
+    elif operation.lower() == 'divide':
+        return float(num1 / num2)
+    else:
+        return 'Invalid operation'
